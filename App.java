374724bee597ef1b65d651ef4b642f03a6bd1f48
hosary-04 <Abdullah2004ash@gmail.com>
2024-12-22 08:56:46 +0200

    Update App.java

--- App.java
+++ App.java
@@ -23,7 +23,7 @@ public class Student {
     }
 public class StudentTest {
 
-    @Test
+    @test
     public void testCalculateGrade() {
         Student student = new Student("Ahmed", 82.0);
         String grade = student.calculateGrade();
