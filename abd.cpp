a7acad4cedf21e791a3801659523d3a42363219e
Your NameAbdullahgit config --global user.name Your <abdullah2004ash@gmail.com>
2024-12-22 04:26:27 +0200

    githup_testing

---
+++ abd.cpp
@@ -0,0 +1,24 @@
+// abd.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+int  sum(int a, int b, int c);
+
+
+
+int main()
+{
+   
+    std::cout << "dear student!\n";
+    std::cout << "lets try new concepts!\n";
+    std::cout << "learning git is fun\n";
+    std::cout << sum(2, 4, 6);
+
+    std::cout << "wf sale\n";
+    std::cout << "by 2 get 3 free\n";
+}
+int sum(int a.int b,int c)
+{
+
+    return a + b + c;
+}
